3dfccaef1310859b671ebbb8b8225ee4fbee1a36
John Dinh <JohnDuyDinh@gmail.com>
2018-06-20 14:51:13 -0400

    Git Branch Lesson

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,10 +4,11 @@ import autoprefixer from 'autoprefixer'
 import cssvars from 'postcss-simple-vars'
 import nested from 'postcss-nested'
 import cssImport from 'postcss-import'
+import mixins from 'postcss-mixins'
 
 gulp.task('styles', () => {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
         .on('error', function (errorInfo) {
             console.log(errorInfo.toString())
             this.emit('end')
